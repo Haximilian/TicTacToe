8715c7ed7734b0bb1f062fd938e9f4022d025cb8
Haximilian <47073524+Haximilian@users.noreply.github.com>
2021-03-26 12:21:04 -0700

    define a winning board state

--- win.pl
+++ win.pl
@@ -1,17 +1,6 @@
 :- use_module(library(clpfd)).
 
-% board domain
-point(1, 1).
-point(1, 2).
-point(1, 3).
-point(2, 1).
-point(2, 2).
-point(2, 3).
-point(3, 1).
-point(3, 2).
-point(3, 3).
-
-% compute board state
+% update board state
 mark([a, _, _, _, _, _, _, _, _], [Player, _, _, _, _, _, _, _, _], Player, 1, 1).
 mark([_, a, _, _, _, _, _, _, _], [_, Player, _, _, _, _, _, _, _], Player, 1, 2).
 mark([_, _, a, _, _, _, _, _, _], [_, _, Player, _, _, _, _, _, _], Player, 1, 3).
@@ -22,3 +11,32 @@ mark([_, _, _, _, _, _, a, _, _], [_, _, _, _, _, _, Player, _, _], Player, 3, 1
 mark([_, _, _, _, _, _, _, a, _], [_, _, _, _, _, _, _, Player, _], Player, 3, 2).
 mark([_, _, _, _, _, _, _, _, a], [_, _, _, _, _, _, _, _, Player], Player, 3, 3).
 
+% extract available positions
+% mark([a, a, a, a, o, a, a, a, a], _, x, X, Y).
+% extract occupied positions
+% mark(_, [a, a, a, a, o, a, a, a, a], o, X, Y).
+
+win(Board, Player) :-
+    mark(_, Board, Player, X, I),
+    mark(_, Board, Player, X, J),
+    mark(_, Board, Player, X, K),
+    J is I + 1,
+    K is J + 1.
+
+% define winning board state
+win(Board, Player) :-
+    mark(_, Board, Player, I, Y),
+    mark(_, Board, Player, J, Y),
+    mark(_, Board, Player, K, Y),
+    J is I + 1,
+    K is J + 1.
+
+win(Board, Player) :-
+    mark(_, Board, Player, 1, 1),
+    mark(_, Board, Player, 2, 2),
+    mark(_, Board, Player, 3, 3).
+
+win(Board, Player) :-
+    mark(_, Board, Player, 1, 3),
+    mark(_, Board, Player, 2, 2),
+    mark(_, Board, Player, 3, 1).
